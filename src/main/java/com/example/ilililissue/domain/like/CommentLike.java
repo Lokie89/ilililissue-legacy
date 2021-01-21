f4088401cb97b5607a967b86b073139f0c122dea
Lokie89 <traeuman@gmail.com>
2021-01-21 12:13:46 +0900

    feat : CommentLike class

--- src/main/java/com/example/ilililissue/domain/like/CommentLike.java
+++ src/main/java/com/example/ilililissue/domain/like/CommentLike.java
@@ -1,2 +1,16 @@
-package com.example.ilililissue.domain.like;public class CommentLike {
+package com.example.ilililissue.domain.like;
+
+import com.example.ilililissue.domain.comment.Comment;
+import com.example.ilililissue.domain.member.Member;
+import lombok.Builder;
+
+@Builder
+public class CommentLike implements Like {
+    private Member member;
+    private Comment comment;
+
+    @Override
+    public int createLike() {
+        return 1;
+    }
 }
