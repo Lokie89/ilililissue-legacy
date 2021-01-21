216da073996a5d141f672a4177f0c8c17ec67e05
Lokie89 <traeuman@gmail.com>
2021-01-21 11:46:00 +0900

    test : CommentTest createIssueCommentTest 수정, Member 추가

--- src/test/java/com/example/ilililissue/domain/comment/CommentTest.java
+++ src/test/java/com/example/ilililissue/domain/comment/CommentTest.java
@@ -9,8 +9,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class CommentTest {
     @Test
     void createIssueCommentTest() {
+        Member issueMember = new IssueMember();
         Issue socialIssue = DefaultIssue.builder().title("신규확진 401명, 이틀째 400명대 초반... 사망자 16명 늘어").images("image", "image2").description("내용").build();
-        Comment issueComment = IssueComment.builder().issue(socialIssue).comment("코로나 스탑!!").build();
+        Comment issueComment = IssueComment.builder().member(issueMember).issue(socialIssue).comment("코로나 스탑!!").build();
         int createdComment = issueComment.createComment();
         assertEquals(1, createdComment);
     }
