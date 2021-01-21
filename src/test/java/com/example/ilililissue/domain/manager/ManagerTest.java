5750300c2bd3d455f77658698836f027b256b391
Lokie89 <traeuman@gmail.com>
2021-01-21 11:55:10 +0900

    test : refactor createIssue test

--- src/test/java/com/example/ilililissue/domain/manager/ManagerTest.java
+++ src/test/java/com/example/ilililissue/domain/manager/ManagerTest.java
@@ -10,9 +10,9 @@ public class ManagerTest {
 
     @Test
     void createIssueTest() {
-        Issue socialIssue = DefaultIssue.builder().title("신규확진 401명, 이틀째 400명대 초반... 사망자 16명 늘어").images("image","image2").description("내용").build();
         Manager manager = new IssueManager();
-        int created = manager.createIssue(socialIssue);
+        Issue socialIssue = DefaultIssue.builder().creator(manager).title("신규확진 401명, 이틀째 400명대 초반... 사망자 16명 늘어").images("image","image2").description("내용").build();
+        int created = socialIssue.createIssue();
 
         assertEquals(1, created);
     }
