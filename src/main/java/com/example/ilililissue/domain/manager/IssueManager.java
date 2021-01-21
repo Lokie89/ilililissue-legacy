ea31816ef733810a59520bbede5fa789919160c8
Lokie89 <traeuman@gmail.com>
2021-01-21 12:53:44 +0900

    refactor : IssueManager Entity 추가

--- src/main/java/com/example/ilililissue/domain/manager/IssueManager.java
+++ src/main/java/com/example/ilililissue/domain/manager/IssueManager.java
@@ -1,4 +1,19 @@
 package com.example.ilililissue.domain.manager;
 
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor
+@Entity
 public class IssueManager implements Manager {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
 }
