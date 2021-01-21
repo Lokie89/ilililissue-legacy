55238493417c4bf420381a8354eddace3b38c369
Lokie89 <traeuman@gmail.com>
2021-01-21 11:26:39 +0900

    feat : IssueComment class

--- src/main/java/com/example/ilililissue/domain/comment/IssueComment.java
+++ src/main/java/com/example/ilililissue/domain/comment/IssueComment.java
@@ -1,2 +1,15 @@
-package com.example.ilililissue.domain.comment;public class IssueComment {
+package com.example.ilililissue.domain.comment;
+
+import com.example.ilililissue.domain.issue.Issue;
+import lombok.Builder;
+
+@Builder
+public class IssueComment implements Comment {
+    private Issue issue;
+    private String comment;
+
+    @Override
+    public int createComment() {
+        return 1;
+    }
 }
