9e9443fe61e8d43ce46cbd0935570d3b60fe95d1
Lokie89 <traeuman@gmail.com>
2021-01-21 11:00:39 +0900

    feat : IssueManager class

--- src/main/java/com/example/ilililissue/domain/manager/IssueManager.java
+++ src/main/java/com/example/ilililissue/domain/manager/IssueManager.java
@@ -1,2 +1,10 @@
-package com.example.ilililissue.domain.manager;public class IssueManager {
+package com.example.ilililissue.domain.manager;
+
+import com.example.ilililissue.domain.issue.Issue;
+
+public class IssueManager implements Manager {
+    @Override
+    public int createIssue(Issue issue) {
+        return 1;
+    }
 }
