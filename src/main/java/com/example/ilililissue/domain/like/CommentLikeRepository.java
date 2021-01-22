5f5ee3f239b125cff49c4c4b216daa05b610d3a7
Lokie89 <traeuman@gmail.com>
2021-01-22 16:22:45 +0900

    feat : CommentLikeRepository extends JpaRepository

---
+++ src/main/java/com/example/ilililissue/domain/like/CommentLikeRepository.java
@@ -0,0 +1,6 @@
+package com.example.ilililissue.domain.like;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentLikeRepository extends JpaRepository<CommentLike, Long> {
+}
