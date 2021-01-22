cee72a52fc538c988f95ffba64ed9c2cf9d8556a
Lokie89 <traeuman@gmail.com>
2021-01-22 15:50:09 +0900

    feat : DefaultIssueRepository extends JpaRepository 추가

---
+++ src/main/java/com/example/ilililissue/domain/issue/DefaultIssueRepository.java
@@ -0,0 +1,6 @@
+package com.example.ilililissue.domain.issue;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DefaultIssueRepository extends JpaRepository<DefaultIssue, Long> {
+}
