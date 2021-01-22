ed81723d868c4abfcbcca2a2c82fe48a1e8576b7
Lokie89 <traeuman@gmail.com>
2021-01-22 18:04:22 +0900

    feat : IssueController

--- src/main/java/com/ilililissue/www/web/controller/IssueController.java
+++ src/main/java/com/ilililissue/www/web/controller/IssueController.java
@@ -1,2 +1,13 @@
-package com.ilililissue.www.web.controller;public class IssueController {
+package com.ilililissue.www.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IssueController {
+
+    @GetMapping("/api/v1/index")
+    public String index(){
+        return "index";
+    }
 }
