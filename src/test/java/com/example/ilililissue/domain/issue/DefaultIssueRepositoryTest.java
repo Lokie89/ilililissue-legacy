1008bd33a768c638b32203956723e3c4261c0e79
Lokie89 <traeuman@gmail.com>
2021-01-22 15:46:55 +0900

    test : DefaultIssueRepositoryTest saveIssueTest 추가

--- src/test/java/com/example/ilililissue/domain/issue/DefaultIssueRepositoryTest.java
+++ src/test/java/com/example/ilililissue/domain/issue/DefaultIssueRepositoryTest.java
@@ -7,8 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import javax.transaction.Transactional;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@Transactional
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(properties = "application-test.properties")
 public class DefaultIssueRepositoryTest {
@@ -17,10 +21,13 @@ public class DefaultIssueRepositoryTest {
 
     @Test
     void saveIssueTest() {
-        IssueManager manager = new IssueManager();
+        IssueManager manager = new IssueManager("manager");
         DefaultIssue defaultIssue = DefaultIssue.builder().creator(manager).title("신규확진 401명, 이틀째 400명대 초반... 사망자 16명 늘어").images("image", "image2").description("내용").build();
         repository.save(defaultIssue);
-        assertEquals(1, defaultIssue.getId());
-        assertEquals(1, manager.getId());
+        List<DefaultIssue> defaultIssueList = repository.findAll();
+        DefaultIssue savedIssue = defaultIssueList.get(0);
+        IssueManager issueManager = savedIssue.getCreator();
+        assertEquals("신규확진 401명, 이틀째 400명대 초반... 사망자 16명 늘어", savedIssue.getTitle());
+        assertEquals("manager", issueManager.getRole());
     }
 }
