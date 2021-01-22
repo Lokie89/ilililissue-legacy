8a751e77a3e59e388f106992df87550ffd0d886c
Lokie89 <traeuman@gmail.com>
2021-01-22 15:45:40 +0900

    feat : IssueManager role 필드 추가

--- src/main/java/com/example/ilililissue/domain/manager/IssueManager.java
+++ src/main/java/com/example/ilililissue/domain/manager/IssueManager.java
@@ -16,4 +16,11 @@ public class IssueManager implements Manager {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     private Long id;
+
+    @Column(name = "role")
+    private String role;
+
+    public IssueManager(String role) {
+        this.role = role;
+    }
 }
