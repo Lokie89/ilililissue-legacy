c08c086e637015e12a15d4b4160b59753cdf678c
Lokie89 <traeuman@gmail.com>
2021-01-22 14:20:19 +0900

    test : entity class 필드 수정

--- src/test/java/com/example/ilililissue/domain/comment/CommentTest.java
+++ src/test/java/com/example/ilililissue/domain/comment/CommentTest.java
@@ -1,9 +1,7 @@
 package com.example.ilililissue.domain.comment;
 
 import com.example.ilililissue.domain.issue.DefaultIssue;
-import com.example.ilililissue.domain.issue.Issue;
 import com.example.ilililissue.domain.member.IssueMember;
-import com.example.ilililissue.domain.member.Member;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -11,8 +9,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class CommentTest {
     @Test
     void createIssueCommentTest() {
-        Member issueMember = new IssueMember();
-        Issue socialIssue = DefaultIssue.builder().title("신규확진 401명, 이틀째 400명대 초반... 사망자 16명 늘어").images("image", "image2").description("내용").build();
+        IssueMember issueMember = new IssueMember();
+        DefaultIssue socialIssue = DefaultIssue.builder().title("신규확진 401명, 이틀째 400명대 초반... 사망자 16명 늘어").images("image", "image2").description("내용").build();
         Comment issueComment = IssueComment.builder().member(issueMember).issue(socialIssue).comment("코로나 스탑!!").build();
         int createdComment = issueComment.createComment();
         assertEquals(1, createdComment);
--- src/test/java/com/example/ilililissue/domain/manager/ManagerTest.java
+++ src/test/java/com/example/ilililissue/domain/manager/ManagerTest.java
@@ -10,8 +10,8 @@ public class ManagerTest {
 
     @Test
     void createIssueTest() {
-        Manager manager = new IssueManager();
-        Issue socialIssue = DefaultIssue.builder().creator(manager).title("신규확진 401명, 이틀째 400명대 초반... 사망자 16명 늘어").images("image","image2").description("내용").build();
+        IssueManager manager = new IssueManager();
+        Issue socialIssue = DefaultIssue.builder().creator(manager).title("신규확진 401명, 이틀째 400명대 초반... 사망자 16명 늘어").images("image", "image2").description("내용").build();
         int created = socialIssue.createIssue();
 
         assertEquals(1, created);
