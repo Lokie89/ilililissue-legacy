a7025fb9780cfc92d808350206aaa1ebac5ef665
Lokie89 <traeuman@gmail.com>
2021-01-22 14:19:54 +0900

    refactor : IssueMember Entity 설정

--- src/main/java/com/example/ilililissue/domain/member/IssueMember.java
+++ src/main/java/com/example/ilililissue/domain/member/IssueMember.java
@@ -1,4 +1,19 @@
 package com.example.ilililissue.domain.member;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor
+@Entity
 public class IssueMember implements Member {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
 }
