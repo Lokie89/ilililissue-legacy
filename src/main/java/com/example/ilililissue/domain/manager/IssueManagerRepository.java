75163c52809fd2f869476f432b009242da8ebc06
Lokie89 <traeuman@gmail.com>
2021-01-22 14:59:28 +0900

    feat : IssueManagerRepository interface extends JpaRepository 추가

---
+++ src/main/java/com/example/ilililissue/domain/manager/IssueManagerRepository.java
@@ -0,0 +1,6 @@
+package com.example.ilililissue.domain.manager;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IssueManagerRepository extends JpaRepository<IssueManager, Long> {
+}
