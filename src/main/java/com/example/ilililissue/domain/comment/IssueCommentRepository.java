99e898807f808c703d89ac351f9b091a7c334870
Lokie89 <traeuman@gmail.com>
2021-01-22 16:08:57 +0900

    feat : IssueCommentRepository extends JpaRepository

---
+++ src/main/java/com/example/ilililissue/domain/comment/IssueCommentRepository.java
@@ -0,0 +1,6 @@
+package com.example.ilililissue.domain.comment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IssueCommentRepository extends JpaRepository<IssueComment, Long> {
+}
