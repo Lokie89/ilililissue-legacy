6a58fec4c16a0ddc358fafb43e5ee694739875c1
Lokie89 <traeuman@gmail.com>
2021-01-26 15:33:45 +0900

    test : transactional javax -> spring

--- src/test/java/com/ilililissue/www/domain/comment/IssueCommentRepositoryTest.java
+++ src/test/java/com/ilililissue/www/domain/comment/IssueCommentRepositoryTest.java
@@ -9,8 +9,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
