41cdaf7ce3c15d05cbc7e62d7389316db6f45d48
Lokie89 <traeuman@gmail.com>
2021-01-26 17:59:19 +0900

    test : IssueComment test 수정

--- src/test/java/com/ilililissue/www/service/comment/IssueCommentServiceTest.java
+++ src/test/java/com/ilililissue/www/service/comment/IssueCommentServiceTest.java
@@ -5,6 +5,7 @@ import com.ilililissue.www.domain.issue.DefaultIssue;
 import com.ilililissue.www.domain.manager.IssueManager;
 import com.ilililissue.www.domain.manager.ManagerRole;
 import com.ilililissue.www.domain.member.IssueMember;
+import com.ilililissue.www.exception.comment.CanNotRegisterCommentException;
 import com.ilililissue.www.service.issue.IssueService;
 import com.ilililissue.www.service.manager.IssueManagerService;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,8 +16,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 @Transactional
 @ExtendWith(SpringExtension.class)
@@ -33,7 +33,8 @@ public class IssueCommentServiceTest {
     IssueService issueService;
 
     @BeforeEach
-    void setUp(){
+    void setUp() {
+        IssueMember issueMember = new IssueMember();
         IssueManager manager = new IssueManager(ManagerRole.MASTER);
         issueManagerService.create(manager);
         DefaultIssue socialIssue = DefaultIssue.builder(manager, "신규확진 401명, 이틀째 400명대 초반... 사망자 16명 늘어").images("image", "image2").description("내용").build();
@@ -41,20 +42,22 @@ public class IssueCommentServiceTest {
     }
 
     @Test
-    void saveIssueComment(){
+    void saveIssueComment() {
 
         IssueMember issueMember = new IssueMember();
         DefaultIssue getIssue = issueService.getAll().get(0);
         IssueComment issueComment = IssueComment.builder().member(issueMember).issue(getIssue).comment("코로나 스탑!!").build();
         issueCommentService.create(issueComment);
-        assertEquals("코로나 스탑!!", issueCommentService.getAll().get(0).getComment().getComment());
+        assertEquals("코로나 스탑!!", issueCommentService.getAll().get(0).getComment());
     }
 
     @Test
-    void cannotRegisterComment(){
+    void cannotRegisterComment() {
         IssueMember issueMember = new IssueMember();
         DefaultIssue getIssue = issueService.getAll().get(0);
-        IssueComment issueComment = IssueComment.builder().member(issueMember).issue(getIssue).comment("코로나 스탑!! 두번째!!!").build();
-        assertThrows(CanNotRegisterCommentException.class, ()->issueCommentService.create(issueComment));
+        IssueComment issueComment1 = IssueComment.builder().member(issueMember).issue(getIssue).comment("코로나 스탑!!").build();
+        issueCommentService.create(issueComment1);
+        IssueComment issueComment2 = IssueComment.builder().member(issueMember).issue(getIssue).comment("코로나 스탑!! 두번째!!!").build();
+        assertThrows(CanNotRegisterCommentException.class, () -> issueCommentService.create(issueComment2));
     }
 }
