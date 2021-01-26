d0dd922b4d58e453753207b17b67e8f0b0c4b180
Lokie89 <traeuman@gmail.com>
2021-01-26 15:29:02 +0900

    feat : IssueManagerService

--- src/main/java/com/ilililissue/www/service/manager/IssueManagerService.java
+++ src/main/java/com/ilililissue/www/service/manager/IssueManagerService.java
@@ -1,2 +1,26 @@
-package com.ilililissue.www.service.manager;public class IssueManagerService {
+package com.ilililissue.www.service.manager;
+
+import com.ilililissue.www.domain.manager.IssueManager;
+import com.ilililissue.www.domain.manager.IssueManagerRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class IssueManagerService {
+
+    private IssueManagerRepository issueManagerRepository;
+
+    public IssueManagerService(IssueManagerRepository issueManagerRepository) {
+        this.issueManagerRepository = issueManagerRepository;
+    }
+
+    public void save(IssueManager issueManager) {
+        issueManagerRepository.save(issueManager);
+    }
+
+    public List<IssueManager> getAll() {
+        return issueManagerRepository.findAll();
+    }
+
 }
