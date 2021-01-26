b933e9035298decd9123ec5349a487d6d51952cc
Lokie89 <traeuman@gmail.com>
2021-01-26 15:40:58 +0900

    feat : IssueService

---
+++ src/main/java/com/ilililissue/www/service/issue/IssueService.java
@@ -0,0 +1,24 @@
+package com.ilililissue.www.service.issue;
+
+import com.ilililissue.www.domain.issue.DefaultIssue;
+import com.ilililissue.www.domain.issue.DefaultIssueRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class IssueService {
+    private DefaultIssueRepository defaultIssueRepository;
+
+    public IssueService(DefaultIssueRepository defaultIssueRepository) {
+        this.defaultIssueRepository = defaultIssueRepository;
+    }
+
+    public void save(DefaultIssue defaultIssue) {
+        defaultIssueRepository.save(defaultIssue);
+    }
+
+    public List<DefaultIssue> getAll() {
+        return defaultIssueRepository.findAll();
+    }
+}
