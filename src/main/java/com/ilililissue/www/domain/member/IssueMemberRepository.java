c6d998fe34b8045cee4db94981aa9ce1ee22c89e
Lokie89 <traeuman@gmail.com>
2021-01-27 10:39:09 +0900

    feat : IssueMemeberRepository

---
+++ src/main/java/com/ilililissue/www/domain/member/IssueMemberRepository.java
@@ -0,0 +1,6 @@
+package com.ilililissue.www.domain.member;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IssueMemberRepository extends JpaRepository<IssueMember, Long> {
+}
