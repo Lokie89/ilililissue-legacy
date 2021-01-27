2e27ddfcf061de1422ff16193f9a1bb1db087190
Lokie89 <traeuman@gmail.com>
2021-01-27 14:54:53 +0900

    feat : IssueCommentService update validate 로직 추가

--- src/main/java/com/ilililissue/www/service/comment/IssueCommentService.java
+++ src/main/java/com/ilililissue/www/service/comment/IssueCommentService.java
@@ -3,6 +3,7 @@ package com.ilililissue.www.service.comment;
 import com.ilililissue.www.domain.comment.IssueComment;
 import com.ilililissue.www.domain.comment.IssueCommentRepository;
 import com.ilililissue.www.exception.comment.CanNotRegisterCommentException;
+import com.ilililissue.www.exception.comment.CanNotUpdateCommentException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -24,10 +25,21 @@ public class IssueCommentService {
     }
 
     private boolean exist(IssueComment issueComment) {
-        return issueCommentRepository.existsByIssueAndMember(issueComment.getIssue(),issueComment.getMember());
+        return issueCommentRepository.existsByIssueAndMember(issueComment.getIssue(), issueComment.getMember());
     }
 
     public List<IssueComment> getAll() {
         return issueCommentRepository.findAll();
+    }
+
+    public void update(IssueComment issueComment) {
+        if (isUpdated(issueComment)) {
+            throw new CanNotUpdateCommentException();
+        }
+        issueCommentRepository.save(issueComment);
+    }
+
+    private boolean isUpdated(IssueComment issueComment) {
+        return issueComment.getModifiedDate() != null;
     }
 }
