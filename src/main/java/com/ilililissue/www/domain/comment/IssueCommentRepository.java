925c655291a8a1818858156ad1ac0fc75f82c2a6
Lokie89 <traeuman@gmail.com>
2021-01-27 11:24:47 +0900

    feat : IssueCommentRepository existsByIssueAndMember

--- src/main/java/com/ilililissue/www/domain/comment/IssueCommentRepository.java
+++ src/main/java/com/ilililissue/www/domain/comment/IssueCommentRepository.java
@@ -1,6 +1,9 @@
 package com.ilililissue.www.domain.comment;
 
+import com.ilililissue.www.domain.issue.DefaultIssue;
+import com.ilililissue.www.domain.member.IssueMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IssueCommentRepository extends JpaRepository<IssueComment, Long> {
+    boolean existsByIssueAndMember(DefaultIssue issue, IssueMember member);
 }
