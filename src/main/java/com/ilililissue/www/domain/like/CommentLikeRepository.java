a4ecc80e2226afdef4a7fbb11d97d5150717ffea
Lokie89 <traeuman@gmail.com>
2021-01-28 15:45:38 +0900

    feat : CommentLikeRepository existsByCommentAndMember 메서드

--- src/main/java/com/ilililissue/www/domain/like/CommentLikeRepository.java
+++ src/main/java/com/ilililissue/www/domain/like/CommentLikeRepository.java
@@ -1,6 +1,9 @@
 package com.ilililissue.www.domain.like;
 
+import com.ilililissue.www.domain.comment.IssueComment;
+import com.ilililissue.www.domain.member.IssueMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentLikeRepository extends JpaRepository<CommentLike, Long> {
+    boolean existsByCommentAndMember(IssueComment comment, IssueMember member);
 }
