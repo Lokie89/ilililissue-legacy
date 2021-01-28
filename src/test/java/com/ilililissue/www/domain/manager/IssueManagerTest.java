9d4da0deb3394629c64bd845eda51536539691be
Lokie89 <traeuman@gmail.com>
2021-01-28 16:47:39 +0900

    refactor : ManagerTest -> IssueManagerTest

--- src/test/java/com/ilililissue/www/domain/manager/IssueManagerTest.java
+++ src/test/java/com/ilililissue/www/domain/manager/IssueManagerTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class ManagerTest {
+public class IssueManagerTest {
 
     @Test
     void createIssueTest() {
