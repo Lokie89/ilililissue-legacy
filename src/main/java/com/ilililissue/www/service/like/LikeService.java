7d3fc4fba454fbe9047bc3e06e288e8a93862b15
Lokie89 <traeuman@gmail.com>
2021-01-28 15:44:39 +0900

    feat : LikeService createOrCancel, getAll

--- src/main/java/com/ilililissue/www/service/like/LikeService.java
+++ src/main/java/com/ilililissue/www/service/like/LikeService.java
@@ -4,6 +4,8 @@ import com.ilililissue.www.domain.like.CommentLike;
 import com.ilililissue.www.domain.like.CommentLikeRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class LikeService {
 
@@ -14,7 +16,27 @@ public class LikeService {
         this.commentLikeRepository = commentLikeRepository;
     }
 
-    public void create(CommentLike commentLike) {
+    private void create(CommentLike commentLike) {
         commentLikeRepository.save(commentLike);
+    }
+
+    public void createOrCancel(CommentLike commentLike) {
+        if (existCommentLike(commentLike)) {
+            if (commentLike.isCanceled()) {
+                commentLike.reLike();
+                return;
+            }
+            commentLike.cancel();
+            return;
+        }
+        create(commentLike);
+    }
+
+    private boolean existCommentLike(CommentLike commentLike) {
+        return commentLikeRepository.existsByCommentAndMember(commentLike.getComment(), commentLike.getMember());
+    }
+
+    public List<CommentLike> getAll() {
+        return commentLikeRepository.findAll();
     }
 }
