a37cc1007d6f3780b2a85b20bb0e0519e117b460
Lokie89 <traeuman@gmail.com>
2021-01-29 17:12:08 +0900

    feat : IssueService create parameter DefaultIssueSaveDto

--- src/main/java/com/ilililissue/www/service/issue/IssueService.java
+++ src/main/java/com/ilililissue/www/service/issue/IssueService.java
@@ -2,6 +2,9 @@ package com.ilililissue.www.service.issue;
 
 import com.ilililissue.www.domain.issue.DefaultIssue;
 import com.ilililissue.www.domain.issue.DefaultIssueRepository;
+import com.ilililissue.www.domain.manager.IssueManager;
+import com.ilililissue.www.service.manager.IssueManagerService;
+import com.ilililissue.www.web.dto.DefaultIssueSaveDto;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,16 +12,25 @@ import java.util.List;
 @Service
 public class IssueService {
     private DefaultIssueRepository defaultIssueRepository;
+    private IssueManagerService issueManagerService;
 
-    public IssueService(DefaultIssueRepository defaultIssueRepository) {
+    public IssueService(DefaultIssueRepository defaultIssueRepository,
+                        IssueManagerService issueManagerService) {
         this.defaultIssueRepository = defaultIssueRepository;
+        this.issueManagerService = issueManagerService;
     }
 
-    public void create(DefaultIssue defaultIssue) {
-        defaultIssueRepository.save(defaultIssue);
+    public DefaultIssue create(DefaultIssue defaultIssue) {
+        return defaultIssueRepository.save(defaultIssue);
     }
 
     public List<DefaultIssue> getAll() {
         return defaultIssueRepository.findAll();
+    }
+
+    public DefaultIssue create(DefaultIssueSaveDto defaultIssueSaveDto) {
+        IssueManager persistManager = issueManagerService.getById(defaultIssueSaveDto.getCreator());
+        DefaultIssue persistIssue = DefaultIssue.builder(persistManager,defaultIssueSaveDto.getTitle()).images(defaultIssueSaveDto.getImages().toArray(String[]::new)).description(defaultIssueSaveDto.getDescription()).build();
+        return defaultIssueRepository.save(persistIssue);
     }
 }
