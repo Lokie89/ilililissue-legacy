b36a072dc228201c16d225362b3befc6387615b2
Lokie89 <traeuman@gmail.com>
2021-01-29 17:04:51 +0900

    feat : IssueController createIssue

--- src/main/java/com/ilililissue/www/web/controller/IssueController.java
+++ src/main/java/com/ilililissue/www/web/controller/IssueController.java
@@ -1,13 +1,29 @@
 package com.ilililissue.www.web.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import com.ilililissue.www.domain.issue.DefaultIssue;
+import com.ilililissue.www.domain.manager.IssueManager;
+import com.ilililissue.www.service.issue.IssueService;
+import com.ilililissue.www.service.manager.IssueManagerService;
+import com.ilililissue.www.web.dto.DefaultIssueSaveDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping(value = "/api/v1/issue")
 @RestController
 public class IssueController {
 
-    @GetMapping("/api/v1/index")
-    public String index(){
-        return "index";
+    private IssueService issueService;
+
+    public IssueController(IssueService issueService) {
+        this.issueService = issueService;
+    }
+
+    @PostMapping("")
+    public ResponseEntity<DefaultIssue> createIssue(@RequestBody DefaultIssueSaveDto issue) {
+        return new ResponseEntity<>(issueService.create(issue), HttpStatus.CREATED);
     }
 }
