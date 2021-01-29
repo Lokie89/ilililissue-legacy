9a4ae453c96ec14dcf7a991f42e856f7b1b1bf17
Lokie89 <traeuman@gmail.com>
2021-01-29 17:42:14 +0900

    feat : DefaultIssueService getById 메서드

--- src/main/java/com/ilililissue/www/service/issue/DefaultIssueService.java
+++ src/main/java/com/ilililissue/www/service/issue/DefaultIssueService.java
@@ -3,11 +3,13 @@ package com.ilililissue.www.service.issue;
 import com.ilililissue.www.domain.issue.DefaultIssue;
 import com.ilililissue.www.domain.issue.DefaultIssueRepository;
 import com.ilililissue.www.domain.manager.IssueManager;
+import com.ilililissue.www.exception.NoContentFromRequestException;
 import com.ilililissue.www.service.manager.IssueManagerService;
 import com.ilililissue.www.web.dto.DefaultIssueSaveDto;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DefaultIssueService {
@@ -30,7 +32,15 @@ public class DefaultIssueService {
 
     public DefaultIssue create(DefaultIssueSaveDto defaultIssueSaveDto) {
         IssueManager persistManager = issueManagerService.getById(defaultIssueSaveDto.getCreator());
-        DefaultIssue persistIssue = DefaultIssue.builder(persistManager,defaultIssueSaveDto.getTitle()).images(defaultIssueSaveDto.getImages().toArray(String[]::new)).description(defaultIssueSaveDto.getDescription()).build();
+        DefaultIssue persistIssue = DefaultIssue.builder(persistManager, defaultIssueSaveDto.getTitle()).images(defaultIssueSaveDto.getImages().toArray(String[]::new)).description(defaultIssueSaveDto.getDescription()).build();
         return repository.save(persistIssue);
+    }
+
+    public DefaultIssue getById(Long id) {
+        Optional<DefaultIssue> optionalDefaultIssue = repository.findById(id);
+        if (optionalDefaultIssue.isPresent()) {
+            return optionalDefaultIssue.get();
+        }
+        throw new NoContentFromRequestException();
     }
 }
