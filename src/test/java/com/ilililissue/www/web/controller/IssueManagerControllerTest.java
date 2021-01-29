8afb4a16ed36d78854bbaffecf575c3d8974e926
Lokie89 <traeuman@gmail.com>
2021-01-29 13:01:47 +0900

    test : IssueManagerControllerTest getManagerTest 추가

--- src/test/java/com/ilililissue/www/web/controller/IssueManagerControllerTest.java
+++ src/test/java/com/ilililissue/www/web/controller/IssueManagerControllerTest.java
@@ -2,15 +2,21 @@ package com.ilililissue.www.web.controller;
 
 import com.ilililissue.www.domain.manager.IssueManager;
 import com.ilililissue.www.domain.manager.ManagerRole;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 
+import java.util.Objects;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @SpringBootTest(properties = "application-test.properties", webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class IssueManagerControllerTest {
 
@@ -18,12 +24,22 @@ public class IssueManagerControllerTest {
     TestRestTemplate restTemplate;
 
     @Test
+    @Order(1)
     void createManagerTest() {
         String url = "/api/v1/issue/manager";
         IssueManager issueManager = new IssueManager(ManagerRole.MASTER);
         HttpEntity<IssueManager> entity = new HttpEntity<>(issueManager);
         ResponseEntity<IssueManager> response = restTemplate.postForEntity(url, entity, IssueManager.class);
         assertEquals(201, response.getStatusCodeValue());
+    }
+
+    @Test
+    @Order(2)
+    void getManagerTest() {
+        String url = "/api/v1/issue/manager/1";
+        ResponseEntity<IssueManager> response = restTemplate.getForEntity(url, IssueManager.class);
+        assertEquals(200, response.getStatusCodeValue());
+        assertEquals(1L, Objects.requireNonNull(response.getBody()).getId());
     }
 
 }
