c0f2b417239a4ae040d6ead3e9cf3a1ca1e44df1
Lokie89 <traeuman@gmail.com>
2021-02-01 16:44:41 +0900

    feat : IssueExceptionHandler handleCannotRegisterCommentException

--- src/main/java/com/ilililissue/www/web/exceptionhandler/IssueExceptionHandler.java
+++ src/main/java/com/ilililissue/www/web/exceptionhandler/IssueExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.ilililissue.www.web.exceptionhandler;
 
+import com.ilililissue.www.exception.comment.CanNotRegisterCommentException;
 import com.ilililissue.www.exception.issue.NotAuthorizedManagerException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,6 +13,12 @@ public class IssueExceptionHandler {
     @ExceptionHandler(value = NotAuthorizedManagerException.class)
     @ResponseStatus(value = HttpStatus.UNAUTHORIZED)
     protected void handleNotAuthorizedManagerException(RuntimeException ex) {
+
+    }
+
+    @ExceptionHandler(value = CanNotRegisterCommentException.class)
+    @ResponseStatus(value = HttpStatus.CONFLICT)
+    protected void handleCannotRegisterCommentException(RuntimeException ex) {
 
     }
 }
