5daeff2d74d965f29c5572d97883d2ddbe11360a
Lokie89 <traeuman@gmail.com>
2021-02-01 10:27:52 +0900

    feat : IssueExceptionHandler handleNotAuthorizedManagerException

--- src/main/java/com/ilililissue/www/web/controller/IssueExceptionHandler.java
+++ src/main/java/com/ilililissue/www/web/controller/IssueExceptionHandler.java
@@ -1,2 +1,17 @@
-package com.ilililissue.www.web.controller;public class IssueExceptionHandler {
+package com.ilililissue.www.web.controller;
+
+import com.ilililissue.www.exception.issue.NotAuthorizedManagerException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice(basePackages = "com.ilililissue.www.web")
+public class IssueExceptionHandler {
+
+    @ExceptionHandler(value = NotAuthorizedManagerException.class)
+    @ResponseStatus(value = HttpStatus.UNAUTHORIZED)
+    protected void handleNotAuthorizedManagerException(RuntimeException ex) {
+
+    }
 }
