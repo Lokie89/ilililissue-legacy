9a1a5b83cb5c5e81fe7e56158607234267ed5226
Lokie89 <traeuman@gmail.com>
2021-02-01 15:44:56 +0900

    test : IssueMemberControllerTest createMemberTest

---
+++ src/test/java/com/ilililissue/www/web/controller/IssueMemberControllerTest.java
@@ -0,0 +1,35 @@
+package com.ilililissue.www.web.controller;
+
+import com.ilililissue.www.domain.member.IssueMember;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Objects;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Transactional
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SpringBootTest(properties = "application-test.properties", webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class IssueMemberControllerTest {
+
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @DisplayName("멤버 생성")
+    @Order(1)
+    @Test
+    void createMemberTest() {
+        String url = "/api/v1/issue/member";
+        IssueMember member = new IssueMember("길동이");
+        HttpEntity<IssueMember> request = new HttpEntity<>(member);
+        ResponseEntity<IssueMember> response = restTemplate.postForEntity(url, request, IssueMember.class);
+        assertEquals(201, response.getStatusCodeValue());
+        assertEquals("길동이", Objects.requireNonNull(response.getBody()).getName());
+    }
+
+}
