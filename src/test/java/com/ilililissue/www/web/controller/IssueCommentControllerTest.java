92607c13c9bf11709375a6a91590d7fb83e107b3
Lokie89 <traeuman@gmail.com>
2021-02-01 15:47:07 +0900

    test : IssueCommentControllerTest createIssueComment

--- src/test/java/com/ilililissue/www/web/controller/IssueCommentControllerTest.java
+++ src/test/java/com/ilililissue/www/web/controller/IssueCommentControllerTest.java
@@ -1,2 +1,66 @@
-package com.ilililissue.www.web.controller;public class IssueCommentControllerTest {
+package com.ilililissue.www.web.controller;
+
+import com.ilililissue.www.domain.comment.IssueComment;
+import com.ilililissue.www.domain.issue.DefaultIssue;
+import com.ilililissue.www.domain.manager.IssueManager;
+import com.ilililissue.www.domain.manager.ManagerRole;
+import com.ilililissue.www.domain.member.IssueMember;
+import com.ilililissue.www.web.dto.DefaultIssueSaveDto;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@Transactional
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SpringBootTest(properties = "application-test.properties", webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class IssueCommentControllerTest {
+
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    private IssueManager createAndGetManager(ManagerRole role) {
+        String url = "/api/v1/issue/manager";
+        IssueManager issueManager = new IssueManager(role);
+        HttpEntity<IssueManager> entity = new HttpEntity<>(issueManager);
+        ResponseEntity<IssueManager> response = restTemplate.postForEntity(url, entity, IssueManager.class);
+        return response.getBody();
+    }
+
+    private DefaultIssue createAndGetDefaultIssue() {
+        IssueManager master = createAndGetManager(ManagerRole.MASTER);
+        String url = "/api/v1/issue";
+        HttpEntity<DefaultIssueSaveDto> request = new HttpEntity<>(DefaultIssueSaveDto.builder().creator(master).title("이슈 제목").images("이미지1", "이미지2").description("반갑습니다.").build());
+        ResponseEntity<DefaultIssue> response = restTemplate.postForEntity(url, request, DefaultIssue.class);
+        return response.getBody();
+    }
+
+    private IssueMember createAndGetIssueMember() {
+        String url = "/api/v1/issue/member";
+        IssueMember member = new IssueMember("댓글러");
+        HttpEntity<IssueMember> request = new HttpEntity<>(member);
+        ResponseEntity<IssueMember> response = restTemplate.postForEntity(url, request, IssueMember.class);
+        return response.getBody();
+    }
+
+    @DisplayName("댓글 만들기")
+    @Order(1)
+    @Test
+    void createIssueComment() {
+        DefaultIssue issue = createAndGetDefaultIssue();
+        IssueMember issueMember = createAndGetIssueMember();
+        String url = "/api/v1/issue/comment";
+        HttpEntity<IssueComment> request = new HttpEntity<>(IssueComment.builder().author(issueMember).issue(issue).comment("노래노래노래노래").build());
+        ResponseEntity<IssueComment> response = restTemplate.postForEntity(url, request, IssueComment.class);
+        assertEquals(201, response.getStatusCodeValue());
+        assertEquals("노래노래노래노래", Objects.requireNonNull(response.getBody()).getComment());
+    }
 }
