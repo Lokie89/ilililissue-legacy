9607d6f6eea9ae7e1e6331c3a98f4686147112ee
Lokie89 <traeuman@gmail.com>
2021-02-01 10:27:21 +0900

    test : IssueControllerTest cannotCreateIssueTest 추가

--- src/test/java/com/ilililissue/www/web/controller/IssueControllerTest.java
+++ src/test/java/com/ilililissue/www/web/controller/IssueControllerTest.java
@@ -5,10 +5,7 @@ import com.ilililissue.www.domain.issue.DefaultIssue;
 import com.ilililissue.www.domain.manager.IssueManager;
 import com.ilililissue.www.domain.manager.ManagerRole;
 import com.ilililissue.www.web.dto.DefaultIssueSaveDto;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -26,18 +23,19 @@ public class IssueControllerTest {
     @Autowired
     TestRestTemplate restTemplate;
 
-    private IssueManager createAndGetManager() {
+    private IssueManager createAndGetManager(ManagerRole role) {
         String url = "/api/v1/issue/manager";
-        IssueManager issueManager = new IssueManager(ManagerRole.MASTER);
+        IssueManager issueManager = new IssueManager(role);
         HttpEntity<IssueManager> entity = new HttpEntity<>(issueManager);
         ResponseEntity<IssueManager> response = restTemplate.postForEntity(url, entity, IssueManager.class);
         return response.getBody();
     }
 
+    @DisplayName("이슈 생성하기")
     @Order(1)
     @Test
     void createIssueTest() {
-        IssueManager master = createAndGetManager();
+        IssueManager master = createAndGetManager(ManagerRole.MASTER);
         String url = "/api/v1/issue";
         HttpEntity<DefaultIssueSaveDto> request = new HttpEntity<>(DefaultIssueSaveDto.builder().creator(master).title("이슈 제목").images("이미지1", "이미지2").description("반갑습니다.").build());
         ResponseEntity<DefaultIssue> response = restTemplate.postForEntity(url, request, DefaultIssue.class);
@@ -45,13 +43,25 @@ public class IssueControllerTest {
         assertEquals(201, response.getStatusCodeValue());
     }
 
+    @DisplayName("생성한 이슈 가져오기")
     @Order(2)
     @Test
     void getIssueTest() {
         String url = "/api/v1/issue/1";
         ResponseEntity<DefaultIssue> response = restTemplate.getForEntity(url, DefaultIssue.class);
-        assertEquals(1, response.getBody().getId());
+        assertEquals(1, Objects.requireNonNull(response.getBody()).getId());
         assertEquals(200, response.getStatusCodeValue());
+    }
+
+    @DisplayName("권한 LV1 일때 이슈 생성 실패 401")
+    @Order(3)
+    @Test
+    void cannotCreateIssueTest() {
+        IssueManager master = createAndGetManager(ManagerRole.LV1);
+        String url = "/api/v1/issue";
+        HttpEntity<DefaultIssueSaveDto> request = new HttpEntity<>(DefaultIssueSaveDto.builder().creator(master).title("이슈 제목").images("이미지1", "이미지2").description("반갑습니다.").build());
+        ResponseEntity<DefaultIssue> response = restTemplate.postForEntity(url, request, DefaultIssue.class);
+        assertEquals(401, response.getStatusCodeValue());
     }
 
 }
