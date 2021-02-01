ea2646b691028de37303d18c23a9003c8e1079fd
Lokie89 <traeuman@gmail.com>
2021-02-01 15:48:30 +0900

    feat : IssueCommentController createIssueComment

--- src/main/java/com/ilililissue/www/web/controller/IssueCommentController.java
+++ src/main/java/com/ilililissue/www/web/controller/IssueCommentController.java
@@ -1,2 +1,25 @@
-package com.ilililissue.www.web.controller;public class IssueCommentController {
+package com.ilililissue.www.web.controller;
+
+import com.ilililissue.www.domain.comment.IssueComment;
+import com.ilililissue.www.service.comment.IssueCommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping(value = "/api/v1/issue/comment")
+@RestController
+public class IssueCommentController {
+
+    private final IssueCommentService issueCommentService;
+
+    @PostMapping(value = "")
+    public ResponseEntity<IssueComment> createIssueComment(@RequestBody IssueComment issueComment) {
+        IssueComment savedIssueComment = issueCommentService.create(issueComment);
+        return new ResponseEntity<>(savedIssueComment, HttpStatus.CREATED);
+    }
 }
