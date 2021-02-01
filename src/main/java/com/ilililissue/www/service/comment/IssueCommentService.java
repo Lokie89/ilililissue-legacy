cef03ba6b1b7e9c4926155e62cbfe6af65535e05
Lokie89 <traeuman@gmail.com>
2021-02-01 15:49:54 +0900

    refactor : IssueCommentService create 로직 객체 내 엔티티 영속성 추가

--- src/main/java/com/ilililissue/www/service/comment/IssueCommentService.java
+++ src/main/java/com/ilililissue/www/service/comment/IssueCommentService.java
@@ -2,26 +2,47 @@ package com.ilililissue.www.service.comment;
 
 import com.ilililissue.www.domain.comment.IssueComment;
 import com.ilililissue.www.domain.comment.IssueCommentRepository;
+import com.ilililissue.www.domain.issue.DefaultIssue;
 import com.ilililissue.www.domain.manager.IssueManager;
 import com.ilililissue.www.domain.member.IssueMember;
 import com.ilililissue.www.exception.comment.CanNotRegisterCommentException;
 import com.ilililissue.www.exception.comment.CanNotRemoveCommentException;
+import com.ilililissue.www.service.issue.DefaultIssueService;
+import com.ilililissue.www.service.member.IssueMemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @RequiredArgsConstructor
 @Service
 public class IssueCommentService {
 
     private final IssueCommentRepository repository;
+    private final IssueMemberService issueMemberService;
+    private final DefaultIssueService defaultIssueService;
 
-    public void create(IssueComment issueComment) {
+    private IssueComment persistInnerIssueComment(IssueComment issueComment) {
+        IssueMember persistAuthor = null;
+        Long authorId = issueComment.getAuthor().getId();
+        if (Objects.nonNull(authorId)) {
+            persistAuthor = issueMemberService.getById(authorId);
+        }
+
+        DefaultIssue persistIssue = null;
+        Long issueId = issueComment.getIssue().getId();
+        if (Objects.nonNull(issueId)) {
+            persistIssue = defaultIssueService.getById(issueId);
+        }
+        return IssueComment.builder().author(persistAuthor).issue(persistIssue).comment(issueComment.getComment()).build();
+    }
+
+    public IssueComment create(IssueComment issueComment) {
         if (exist(issueComment)) {
             throw new CanNotRegisterCommentException();
         }
-        repository.save(issueComment);
+        return repository.save(persistInnerIssueComment(issueComment));
     }
 
     private boolean exist(IssueComment issueComment) {
