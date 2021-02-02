d2a2fb14fe2dd4ef5e2a7d41c7c8eea9d89d5d58
Lokie89 <traeuman@gmail.com>
2021-02-02 15:44:15 +0900

    test : IssueManagerControllerTest refactor

--- src/test/java/com/ilililissue/www/web/controller/IssueManagerControllerTest.java
+++ src/test/java/com/ilililissue/www/web/controller/IssueManagerControllerTest.java
@@ -1,44 +1,68 @@
 package com.ilililissue.www.web.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ilililissue.www.domain.manager.IssueManager;
 import com.ilililissue.www.domain.manager.ManagerRole;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.ResponseEntity;
-
-import java.util.Objects;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @SpringBootTest(properties = "application-test.properties", webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class IssueManagerControllerTest {
 
     @Autowired
-    TestRestTemplate restTemplate;
+    WebApplicationContext applicationContext;
 
-    @DisplayName("매니저 생성")
-    @Test
-    @Order(1)
-    void createManagerTest() {
-        String url = "/api/v1/manager";
-        IssueManager issueManager = new IssueManager(ManagerRole.MASTER);
-        HttpEntity<IssueManager> entity = new HttpEntity<>(issueManager);
-        ResponseEntity<IssueManager> response = restTemplate.postForEntity(url, entity, IssueManager.class);
-        assertEquals(201, response.getStatusCodeValue());
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(applicationContext)
+                .apply(springSecurity())
+                .build()
+        ;
     }
 
+    @Order(1)
+    @DisplayName("매니저 생성")
+    @Test
+    void createManagerTest() throws Exception {
+        String url = "/api/v1/manager";
+        IssueManager issueManager = new IssueManager(ManagerRole.MASTER);
+        MvcResult response = mockMvc
+                .perform(post(url)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsBytes(issueManager))
+                )
+                .andReturn();
+        assertEquals(201, response.getResponse().getStatus());
+    }
+
+    @Order(2)
     @DisplayName("매니저 가져오기")
     @Test
-    @Order(2)
-    void getManagerTest() {
+    void getManagerTest() throws Exception {
         String url = "/api/v1/manager/1";
-        ResponseEntity<IssueManager> response = restTemplate.getForEntity(url, IssueManager.class);
-        assertEquals(200, response.getStatusCodeValue());
-        assertEquals(1L, Objects.requireNonNull(response.getBody()).getId());
+        MvcResult response = mockMvc
+                .perform(get(url))
+                .andReturn();
+        IssueManager savedManager = new ObjectMapper().readValue(response.getResponse().getContentAsString(), IssueManager.class);
+        assertEquals(200, response.getResponse().getStatus());
+        assertEquals(1L, savedManager.getId());
     }
 
 }
