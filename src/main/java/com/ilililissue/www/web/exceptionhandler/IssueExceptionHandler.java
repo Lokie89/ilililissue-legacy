8828696bf94da2005c38e003fe55455e996d8385
Lokie89 <traeuman@gmail.com>
2021-02-02 11:22:49 +0900

    feat : IssueExceptionHandler handleCannotUpdateCommentException

--- src/main/java/com/ilililissue/www/web/exceptionhandler/IssueExceptionHandler.java
+++ src/main/java/com/ilililissue/www/web/exceptionhandler/IssueExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.ilililissue.www.web.exceptionhandler;
 
 import com.ilililissue.www.exception.comment.CanNotRegisterCommentException;
+import com.ilililissue.www.exception.comment.CanNotUpdateCommentException;
 import com.ilililissue.www.exception.issue.NotAuthorizedManagerException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -19,6 +20,12 @@ public class IssueExceptionHandler {
     @ExceptionHandler(value = CanNotRegisterCommentException.class)
     @ResponseStatus(value = HttpStatus.CONFLICT)
     protected void handleCannotRegisterCommentException(RuntimeException ex) {
+
+    }
+
+    @ExceptionHandler(value = CanNotUpdateCommentException.class)
+    @ResponseStatus(value = HttpStatus.METHOD_NOT_ALLOWED)
+    protected void handleCannotUpdateCommentException() {
 
     }
 }
