6a8293ac29e7796a9d602da36b5e5c0b200def8b
Lokie89 <traeuman@gmail.com>
2021-02-03 14:31:26 +0900

    test : CommentLikeControllerTest createLikeTest

---
+++ src/test/java/com/ilililissue/www/web/controller/CommentLikeControllerTest.java
@@ -0,0 +1,91 @@
+package com.ilililissue.www.web.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ilililissue.www.domain.comment.IssueComment;
+import com.ilililissue.www.domain.issue.DefaultIssue;
+import com.ilililissue.www.domain.like.CommentLike;
+import com.ilililissue.www.domain.manager.IssueManager;
+import com.ilililissue.www.domain.manager.ManagerRole;
+import com.ilililissue.www.domain.member.IssueMember;
+import com.ilililissue.www.service.comment.IssueCommentService;
+import com.ilililissue.www.service.issue.DefaultIssueService;
+import com.ilililissue.www.service.manager.IssueManagerService;
+import com.ilililissue.www.service.member.IssueMemberService;
+import com.ilililissue.www.web.dto.DefaultIssueSaveDto;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@DisplayName("좋아요 컨트롤러 테스트")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SpringBootTest(properties = "application-test.properties", webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CommentLikeControllerTest {
+
+    @Autowired
+    WebApplicationContext applicationContext;
+
+    @Autowired
+    IssueManagerService issueManagerService;
+
+    @Autowired
+    DefaultIssueService defaultIssueService;
+
+    @Autowired
+    IssueMemberService issueMemberService;
+
+    @Autowired
+    IssueCommentService issueCommentService;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(applicationContext)
+                .addFilters(new CharacterEncodingFilter("UTF-8", true))
+                .apply(springSecurity())
+                .build()
+        ;
+    }
+
+    @DisplayName("세팅")
+    @Order(1)
+    @Test
+    void createComment() {
+        IssueManager master = issueManagerService.create(new IssueManager(ManagerRole.MASTER));
+        DefaultIssueSaveDto issueSaveDto = DefaultIssueSaveDto.builder().creator(master).title("타이를").build();
+        defaultIssueService.create(issueSaveDto);
+        IssueMember member = new IssueMember("코멘터");
+        issueMemberService.create(member);
+        DefaultIssue savedIssue = defaultIssueService.toEntity(1L);
+        IssueComment issueComment = IssueComment.builder().issue(savedIssue).author(member).comment("코멘트").build();
+        issueCommentService.create(issueComment);
+    }
+
+    @DisplayName("좋아요 생성 테스트")
+    @Order(2)
+    @Test
+    void createLikeTest() throws Exception {
+        IssueComment issueComment = issueCommentService.toEntity(1L);
+        IssueMember liker = issueMemberService.create(new IssueMember("라이커"));
+        CommentLike commentLike = CommentLike.builder().comment(issueComment).member(liker).build();
+        String url = "/api/v1/like";
+        MvcResult response = mockMvc
+                .perform(post(url)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsBytes(commentLike))
+                )
+                .andReturn();
+        assertEquals(201, response.getResponse().getStatus());
+    }
+}
