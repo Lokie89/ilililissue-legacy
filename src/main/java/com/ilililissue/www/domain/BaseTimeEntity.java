a90bf6736a43b98ad1aabfa561c4bfecb2477c38
Lokie89 <traeuman@gmail.com>
2021-02-03 11:18:32 +0900

    refactor : BaseTimeEntity field JsonDeserialize

--- src/main/java/com/ilililissue/www/domain/BaseTimeEntity.java
+++ src/main/java/com/ilililissue/www/domain/BaseTimeEntity.java
@@ -1,5 +1,7 @@
 package com.ilililissue.www.domain;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -13,9 +15,13 @@ import java.time.LocalDateTime;
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTimeEntity {
+
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     @CreatedDate
     private LocalDateTime createdDate;
 
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     @LastModifiedDate
     private LocalDateTime modifiedDate;
+
 }
