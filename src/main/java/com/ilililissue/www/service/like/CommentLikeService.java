71b1ee6320144fde79de2c35fdbd213ded274da3
Lokie89 <traeuman@gmail.com>
2021-02-03 14:32:48 +0900

    fix : CommentLikeService 영속성없는 객체 저장시 안의 엔티티 영속화 로직 추가

--- src/main/java/com/ilililissue/www/service/like/CommentLikeService.java
+++ src/main/java/com/ilililissue/www/service/like/CommentLikeService.java
@@ -1,9 +1,13 @@
 package com.ilililissue.www.service.like;
 
+import com.ilililissue.www.domain.comment.IssueComment;
 import com.ilililissue.www.domain.like.CommentLike;
 import com.ilililissue.www.domain.like.CommentLikeRepository;
+import com.ilililissue.www.domain.member.IssueMember;
 import com.ilililissue.www.exception.CanNotBecomeEntityException;
 import com.ilililissue.www.exception.NoContentFromRequestException;
+import com.ilililissue.www.service.comment.IssueCommentService;
+import com.ilililissue.www.service.member.IssueMemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,19 +20,28 @@ import java.util.Objects;
 public class CommentLikeService {
 
     private final CommentLikeRepository repository;
+    private final IssueMemberService issueMemberService;
+    private final IssueCommentService issueCommentService;
 
-    private void create(CommentLike commentLike) {
-        repository.save(commentLike);
+    private CommentLike create(CommentLike commentLike) {
+        return repository.save(commentLike);
+    }
+
+    private CommentLike persistInnerCommentLike(CommentLike notPersistCommentLike) {
+        IssueMember issueMember = issueMemberService.toEntity(notPersistCommentLike.getMember());
+        IssueComment issueComment = issueCommentService.toEntity(notPersistCommentLike.getComment());
+        return CommentLike.builder().member(issueMember).comment(issueComment).build();
     }
 
     @Transactional
-    public void createOrCancel(CommentLike commentLike) {
-        if (existCommentLike(commentLike)) {
-            CommentLike persistCommentLike = getOneByCommentAndMember(commentLike);
+    public CommentLike createOrCancel(CommentLike commentLike) {
+        CommentLike persistInnerCommentLike = persistInnerCommentLike(commentLike);
+        if (existCommentLike(persistInnerCommentLike)) {
+            CommentLike persistCommentLike = getOneByCommentAndMember(persistInnerCommentLike);
             persistCommentLike.likeOrCancel();
-            return;
+            return persistCommentLike;
         }
-        create(commentLike);
+        return create(persistInnerCommentLike);
     }
 
     private boolean existCommentLike(CommentLike commentLike) {
@@ -39,11 +52,11 @@ public class CommentLikeService {
         return repository.findByCommentAndMember(commentLike.getComment(), commentLike.getMember());
     }
 
-    public CommentLike toEntity(Long id){
+    public CommentLike toEntity(Long id) {
         return repository.findById(id).orElseThrow(NoContentFromRequestException::new);
     }
 
-    public CommentLike toEntity(CommentLike notPersistCommentLike){
+    public CommentLike toEntity(CommentLike notPersistCommentLike) {
         if (Objects.isNull(notPersistCommentLike.getId())) {
             throw new CanNotBecomeEntityException();
         }
