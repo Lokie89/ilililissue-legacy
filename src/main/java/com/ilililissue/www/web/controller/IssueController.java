41f0732053a39dc761dcc7b784ea7fc85148623d
Lokie89 <traeuman@gmail.com>
2021-02-04 16:08:21 +0900

    refactor : DefaultIssueController createIssue session 확인 로직 추가

--- src/main/java/com/ilililissue/www/web/controller/IssueController.java
+++ src/main/java/com/ilililissue/www/web/controller/IssueController.java
@@ -1,12 +1,15 @@
 package com.ilililissue.www.web.controller;
 
 import com.ilililissue.www.domain.issue.DefaultIssue;
+import com.ilililissue.www.domain.manager.IssueManager;
 import com.ilililissue.www.service.issue.DefaultIssueService;
 import com.ilililissue.www.web.dto.DefaultIssueSaveDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RequiredArgsConstructor
 @RequestMapping(value = "/api/v1/issue")
@@ -16,7 +19,9 @@ public class IssueController {
     private final DefaultIssueService defaultIssueService;
 
     @PostMapping("")
-    public ResponseEntity<DefaultIssue> createIssue(@RequestBody DefaultIssue issue) {
+    public ResponseEntity<DefaultIssue> createIssue(HttpServletRequest request, @RequestBody DefaultIssueSaveDto issueSaveDto) {
+        IssueManager creator = (IssueManager) request.getSession().getAttribute("sign");
+        DefaultIssue issue = issueSaveDto.toEntity(creator);
         return new ResponseEntity<>(defaultIssueService.create(issue), HttpStatus.CREATED);
     }
 
