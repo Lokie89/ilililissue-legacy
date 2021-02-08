56b31c835bcc9bb2e92f398c9a590ebeb1e0e6ea
Lokie89 <traeuman@gmail.com>
2021-02-08 15:20:21 +0900

    feat : IssueCommentController getIssueCommentList

--- src/main/java/com/ilililissue/www/web/restcontroller/IssueCommentController.java
+++ src/main/java/com/ilililissue/www/web/restcontroller/IssueCommentController.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @RequestMapping(value = "/api/v1/comment")
 @RestController
@@ -36,5 +38,10 @@ public class IssueCommentController {
         IssueComment issueComment = issueCommentDeleteDto.getIssueComment();
         issueCommentService.remove(issueComment, author);
         return new ResponseEntity<>(issueComment, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "")
+    public ResponseEntity<List<IssueComment>> getIssueCommentList() {
+        return new ResponseEntity<>(issueCommentService.getAll(), HttpStatus.OK);
     }
 }
