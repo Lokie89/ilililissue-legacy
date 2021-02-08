7f75bb9f1db8c9e15da812bd883bf6c3ef8c4e15
Lokie89 <traeuman@gmail.com>
2021-02-08 15:20:51 +0900

    refactor : dayissue.mustache 이미지, 댓글 가운데 정렬

--- src/main/resources/static/js/scrollcomment.js
+++ src/main/resources/static/js/scrollcomment.js
@@ -6,7 +6,7 @@ $(function () {
 
 function scrollEvent() {
     $(window).scroll(function () {
-        if ($(window).scrollTop() == $(document).height() - $(window).height()) {
+        if ($(window).scrollTop() === $(document).height() - $(window).height()) {
             loadComment(++page);
         }
     })
@@ -14,11 +14,56 @@ function scrollEvent() {
 
 function loadComment(page) {
 
-    $.get('/api/v1/comment' + '?page=' + page, (commentList) => {
+    // let issueComment = {
+    //     author : {
+    //         id : 4,
+    //     },
+    //     issue : {
+    //         id : 1,
+    //     },
+    //     comment : '4444',
+    //     position : 'ABSTENTION',
+    // }
+    //
+    // let issueMember = {
+    //     name : '작성자44',
+    // }
+    //
+    // $.ajax({
+    //     url : '/api/v1/member',
+    //     type : 'post',
+    //     contentType : 'application/json',
+    //     data : JSON.stringify(issueMember),
+    // });
+    //
+    // $.ajax({
+    //     url : '/api/v1/comment',
+    //     type : 'post',
+    //     contentType : 'application/json',
+    //     data : JSON.stringify(issueComment),
+    // });
+
+    $.get('/api/v1/comment' /*+ '?page=' + page*/, (commentList) => {
         renderComment(commentList);
     })
 }
 
 function renderComment(commentList) {
-    $('#comment').append('야!');
+    const front_left_div = '<div class="comment left">';
+    const front_right_div = '<div class="comment right">';
+    const front_center_div = '<div class="comment center">';
+    const end_div = '</div>';
+    commentList.forEach(comment => {
+        let appendHtml = ''
+        if (comment.position === 'AGREE') {
+            appendHtml += front_left_div;
+        } else if (comment.position === 'DISAGREE') {
+            appendHtml += front_right_div;
+        } else {
+            appendHtml += front_center_div;
+        }
+        appendHtml += comment.comment;
+        appendHtml += end_div;
+        $('#comment').append(appendHtml);
+    })
 }
