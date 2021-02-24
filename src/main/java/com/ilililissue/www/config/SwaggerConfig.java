a7dadede674b4a8305f220318651950c888b8e93
Lokie89 <traeuman@gmail.com>
2021-02-25 02:27:44 +0900

    feat : SwagggerConfig

--- src/main/java/com/ilililissue/www/config/SwaggerConfig.java
+++ src/main/java/com/ilililissue/www/config/SwaggerConfig.java
@@ -1,5 +1,38 @@
-package com.ilililissue.www.config;/**
+package com.ilililissue.www.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
  * @author SeongRok.Oh
  * @since 2021/02/25
-*/public class SwaggerConfig {
+ */
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket apiDocket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.ilililissue.www"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("IlililIssue API Swagger Documentation")
+                .version("1.0")
+                .licenseUrl("https://github.com/Lokie89/ilililissue")
+                .build();
+    }
+
 }
